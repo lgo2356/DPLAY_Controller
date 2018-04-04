25ab8958a478ed48fe455b480473b293ddb265ce
전승훈 <hun@jeonseunghun-ui-MacBook-Pro.local>
2018-04-04 18:19:45 +0900

    Completed layout

--- DPLAY_Controller/app/src/main/java/com/example/hun/myapplication/MainActivity.java
+++ DPLAY_Controller/app/src/main/java/com/example/hun/myapplication/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     private static final int REQUEST_CODE_LED = 101;
     private static final int REQUEST_CODE_SENSOR = 102;
+    private static final int REQUEST_CODE_BUZZER = 103;
+    private static final int REQUEST_CODE_MOTOR = 104;
+    private static final int REQUEST_CODE_BUTTON = 105;
+    private static final int REQUEST_CODE_SETTINGS = 106;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,10 @@ public class MainActivity extends Activity {
 
         Button btnLED = (Button) findViewById(R.id.btnLED);
         Button btnSensor = (Button) findViewById(R.id.btnSensor);
+        Button btnBuzzer = (Button) findViewById(R.id.btnBuzzer);
+        Button btnMotor = (Button) findViewById(R.id.btnMotor);
+        Button btnButton = (Button) findViewById(R.id.btnButton);
+        Button btnSettings = (Button) findViewById(R.id.btnSettings);
 
         btnLED.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +42,38 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SensorActivity.class);
                 startActivityForResult(intent, REQUEST_CODE_SENSOR);
+            }
+        });
+
+        btnBuzzer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BuzzerActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_BUZZER);
+            }
+        });
+
+        btnMotor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MotorActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_MOTOR);
+            }
+        });
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ButtonActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_BUTTON);
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SETTINGS);
             }
         });
     }
